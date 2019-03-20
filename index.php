cb4cc33ca7d9eb04a2188ad3679a5028071e9de1
Jailton Dantas <jailton.dantass@gmail.com>
2019-03-19 21:11:04 -0300

    add usuario

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$jogos = new Usuario();
 
-$usuarios =  $sql->select("SELECT * FROM tb_usuarios");
+$jogos->loadById(3);
 
-echo json_encode($usuarios);
+echo $jogos;
 
 ?>
